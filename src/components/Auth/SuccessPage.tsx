7bfb7dce14785e6c1fbf5d6018e36f56e0a8c709
moizahmedd <ahmedmoiz854@gmail.com>
2024-10-23 16:13:35 -0400

    show wallet address on successful auth

--- src/components/Auth/SuccessPage.tsx
+++ src/components/Auth/SuccessPage.tsx
@@ -1,10 +1,14 @@
 // src/components/SuccessPage.tsx
 import React from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 const SuccessPage: React.FC = () => {
+  const { user } = useAuthContext(); // Get verifyOtp from the context
   return (
     <div className="text-green-500 text-lg">
       Authentication successful! You are logged in.
+      <br></br>
+      Wallet Address:{user?.walletAddress}
     </div>
   );
 };
