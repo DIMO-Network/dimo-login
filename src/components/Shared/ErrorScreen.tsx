fb00e4614d2744b7d33eaeb4a18d5073f14c3744
moizahmedd <ahmedmoiz854@gmail.com>
2024-11-05 13:00:55 -0500

    introduce new shared components for loading, errors, logo, and add styling files

---
+++ src/components/Shared/ErrorScreen.tsx
@@ -0,0 +1,24 @@
+// components/Shared/ErrorScreen.js
+import React from "react";
+import Logo from "./Logo";
+
+interface ErrorScreenProps {
+    title: string;
+    message: string;
+  }
+
+const ErrorScreen: React.FC<ErrorScreenProps> = ({ title, message }) => (
+  <div className="flex h-screen items-center justify-center bg-gray-100">
+    <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-xl">
+      <div className="flex justify-center mb-4">
+        <Logo />
+      </div>
+      <h1 className="text-xl font-bold mb-4" style={{ color: "#E80303" }}>
+        {title}
+      </h1>
+      <p>{message}</p>
+    </div>
+  </div>
+);
+
+export default ErrorScreen;
---
+++ src/components/Shared/LoadingScreen.tsx
@@ -0,0 +1,17 @@
+// components/Shared/LoadingScreen.js
+import React from "react";
+import Logo from "./Logo";
+import Loader from "./Loader";
+
+const LoadingScreen = () => (
+  <div className="flex h-screen items-center justify-center bg-gray-100">
+    <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+      <div className="flex justify-center mb-4">
+        <Logo />
+      </div>
+      <Loader />
+    </div>
+  </div>
+);
+
+export default LoadingScreen;
