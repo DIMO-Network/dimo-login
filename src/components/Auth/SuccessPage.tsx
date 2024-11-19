e8adf6ebe7aed805720de7fb8645a8a1643b4f26
moizahmedd <ahmedmoiz854@gmail.com>
2024-11-19 14:43:24 -0500

    continue button on success

--- src/components/Auth/SuccessPage.tsx
+++ src/components/Auth/SuccessPage.tsx
@@ -3,18 +3,34 @@ import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
 import Card from "../Shared/Card";
 import Header from "../Shared/Header";
+import PrimaryButton from "../Shared/PrimaryButton";
+import { useDevCredentials } from "../../context/DevCredentialsContext";
+import { sendTokenToParent } from "../../utils/authUtils";
 
 const SuccessPage: React.FC = () => {
-  const { user } = useAuthContext(); // Get verifyOtp from the context
+  const { user, jwt } = useAuthContext(); // Should be set on session init
+  const { redirectUri, setUiState } = useDevCredentials();
+
+  const sendJwtAfterPermissions = () => {
+    if (jwt && redirectUri) {
+      sendTokenToParent(jwt, redirectUri, () => {
+        setUiState("SUCCESS");
+      });
+    }
+  };
+
+  const handleContinue = () => {
+    sendJwtAfterPermissions();
+  };
   return (
     <Card width="w-full max-w-[600px]" height="h-full max-h-[308px]">
-      <Header
-        title="You are logged in!"
-        subtitle={
-          user ? user.email : ""
-        }
-      />
-      </Card>
+      <Header title="You are logged in!" subtitle={user ? user.email : ""} />
+      <div className="flex justify-center">
+        <PrimaryButton onClick={handleContinue} width="w-[214px]">
+          Continue
+        </PrimaryButton>
+      </div>
+    </Card>
   );
 };
 
