afcfbc5c932a5164eeacd7f5aa831b96479db3bc
moizahmedd <ahmedmoiz854@gmail.com>
2024-11-30 18:21:56 -0500

    fix paddings

--- src/components/Shared/ErrorScreen.tsx
+++ src/components/Shared/ErrorScreen.tsx
@@ -8,7 +8,7 @@ interface ErrorScreenProps {
 }
 
 const ErrorScreen: React.FC<ErrorScreenProps> = ({ title, message }) => (
-  <div className="flex h-screen pt-10 lg:pt-40 justify-center bg-white lg:bg-[#F7F7F7]">
+  <div className="flex h-screen pt-2 lg:pt-40 justify-center bg-white lg:bg-[#F7F7F7]">
     <div className="bg-white p-8 rounded-lg text-center max-w-[600px] w-full h-full max-h-[308px]">
       <div className="flex justify-center mb-4">
         <Logo />
--- src/components/Shared/LoadingScreen.tsx
+++ src/components/Shared/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 import Loader from "./Loader";
 
 const LoadingScreen = () => (
-  <div className="flex h-screen pt-10 lg:pt-40 justify-center bg-white lg:bg-[#F7F7F7]">
+  <div className="flex h-screen pt-2 lg:pt-40 justify-center bg-white lg:bg-[#F7F7F7]">
     <div className="bg-white p-8 rounded-lg text-center max-w-[600px] w-full h-full max-h-[308px]">
       <div className="flex justify-center mb-4">
         <Logo />
