d3938ff1ab2fdeda866db280af4672c29b03e67e
moizahmedd <ahmedmoiz854@gmail.com>
2024-12-05 15:23:57 -0500

    add logout button to success page

--- src/components/Auth/SuccessPage.tsx
+++ src/components/Auth/SuccessPage.tsx
@@ -5,31 +5,51 @@ import Card from "../Shared/Card";
 import Header from "../Shared/Header";
 import PrimaryButton from "../Shared/PrimaryButton";
 import { useDevCredentials } from "../../context/DevCredentialsContext";
-import { buildAuthPayload, sendAuthPayloadToParent } from "../../utils/authUtils";
+import {
+  buildAuthPayload,
+  logout,
+  sendAuthPayloadToParent,
+} from "../../utils/authUtils";
 
 const SuccessPage: React.FC = () => {
   const { user, jwt } = useAuthContext(); // Should be set on session init
-  const { redirectUri, setUiState, clientId, devLicenseAlias } = useDevCredentials();
+  const { redirectUri, setUiState, clientId, devLicenseAlias } =
+    useDevCredentials();
 
   const sendJwtAfterPermissions = () => {
     if (jwt && redirectUri && clientId) {
       const authPayload = buildAuthPayload(clientId, jwt, user);
       sendAuthPayloadToParent(authPayload, redirectUri, () => {
         setUiState("SUCCESS");
-      })
+      });
     }
   };
 
   const handleContinue = () => {
     sendJwtAfterPermissions();
   };
+
+  const handleLogout = () => {
+    if (redirectUri && clientId) {
+      logout(clientId, redirectUri);
+    }
+  };
+
   return (
     <Card width="w-full max-w-[600px]" height="h-full max-h-[308px]">
       <Header title="You are logged in!" subtitle={user ? user.email : ""} />
-      <div className="flex justify-center">
-        <PrimaryButton onClick={handleContinue} width="w-[214px]">
-          Back to {devLicenseAlias}
-        </PrimaryButton>
+
+      <div className="space-y-4">
+        <div className="flex justify-center">
+          <PrimaryButton onClick={handleContinue} width="w-64">
+            Back to {devLicenseAlias}
+          </PrimaryButton>
+        </div>
+        <div className="flex justify-center">
+          <PrimaryButton onClick={handleLogout} width="w-32">
+            Logout
+          </PrimaryButton>
+        </div>
       </div>
     </Card>
   );
