3be72dd43c3b1a3d727f66e7d1ee2efef738c431
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-12-24 08:54:34 -0500

    added: className in card component

--- src/components/Shared/Card.tsx
+++ src/components/Shared/Card.tsx
@@ -1,11 +1,19 @@
 interface CardProps {
   width: string;
   height: string;
+  className?: string;
   children: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ width, height, children }) => (
-  <div className={`${width} ${height} pt-6 px-8 lg:px-4 pb-10 rounded-2xl bg-white`}>
+const Card: React.FC<CardProps> = ({
+  width,
+  height,
+  className = "",
+  children,
+}) => (
+  <div
+    className={`${width} ${height} pt-6 px-8 lg:px-4 pb-10 rounded-2xl bg-white ${className}`}
+  >
     {children}
   </div>
 );
