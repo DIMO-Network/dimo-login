4ee6af95d2edccce12e2e51f6d3a1a34237d5bc4
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-01-26 14:00:19 -0500

    feat: added function to get the login text

--- src/utils/txnUtils.ts
+++ src/utils/txnUtils.ts
@@ -1,18 +1,26 @@
+import { HEADERS } from "../enums";
 import { sendMessageToReferrer } from "./messageHandler";
 
-export function sendTxnResponseToParent(
+export const sendTxnResponseToParent = (
   transactionHash: string,
   token: string,
   onSuccess: (transactionHash: string) => void
-) {
+) => {
   //This simply sends the transaction response to the developer
   //We don't deal with closing the popup or redirecting, as that's handled by the SuccessfulTransaction screen
   sendMessageToReferrer({
     eventType: "transactionResponse",
     token,
     transactionHash,
-  });  
+  });
 
   onSuccess(transactionHash);
+};
 
-}
+export const getSignInTitle = (
+  alias: string,
+  { altText = false }: { altText?: boolean } = {}
+) => {
+  const titleBaseText = altText ? HEADERS.ALT : HEADERS.DEFAULT;
+  return titleBaseText.replace("%s", alias);
+};
