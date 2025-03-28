34a65f174d98c6edbddd1445ce3d1542fc8c08db
moizahmedd <ahmedmoiz854@gmail.com>
2025-03-28 11:42:43 -0400

    configure oracle context

---
+++ src/context/OraclesContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, ReactNode, useContext, useState } from "react";
+
+interface GlobalOraclesContextProps {
+  onboardingEnabled: boolean;
+  setOnboardingEnabled: (enabled: boolean) => void;
+}
+
+const GlobalOraclesContext = createContext<GlobalOraclesContextProps | undefined>(undefined);
+
+export const GlobalOraclesProvider = ({ children }: { children: ReactNode }) => {
+  const [onboardingEnabled, setOnboardingEnabled] = useState<boolean>(false);
+
+  return (
+    <GlobalOraclesContext.Provider value={{ onboardingEnabled, setOnboardingEnabled }}>
+      {children}
+    </GlobalOraclesContext.Provider>
+  );
+};
+
+export const useOracles = () => {
+  const context = useContext(GlobalOraclesContext);
+  if (!context) {
+    throw new Error("useOracles must be used within a GlobalOraclesProvider");
+  }
+  return context;
+};
