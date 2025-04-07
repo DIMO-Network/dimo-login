ee416e9a398e1b161489d60ab8d363cf40e11622
Neil Devas <neil.devas97@gmail.com>
2025-04-07 16:13:02 -0400

    Update vehicleService.test.ts

--- src/services/__tests__/vehicleService.test.ts
+++ src/services/__tests__/vehicleService.test.ts
@@ -1,7 +1,7 @@
 jest.mock('../identityService');
 
 import { fetchVehiclesWithTransformation } from '../vehicleService';
-import { fetchVehicles } from '../identityService';
+import { fetchVehicles, getPowertrainTypeMatch } from '../identityService';
 
 beforeEach(() => {
   (fetchVehicles as jest.Mock).mockImplementation(() => {
@@ -44,6 +44,16 @@ beforeEach(() => {
       },
     });
   });
+  (getPowertrainTypeMatch as jest.Mock).mockImplementation(
+    (vehicle: any, powertrainTypes: string[]) => {
+      if (vehicle.definition.id === 'tesla_model_3_2019') {
+        return powertrainTypes.includes('BEV');
+      }
+      if (vehicle.definition.id === 'ford_bronco_2023') {
+        return powertrainTypes.includes('ICE');
+      }
+    },
+  );
 });
 
 afterEach(() => jest.restoreAllMocks());
@@ -85,4 +95,63 @@ it('Only returns vehicles that match the make', async () => {
   expect(data.compatibleVehicles[0].make).toEqual('Ford');
   expect(data.incompatibleVehicles.length).toEqual(1);
   expect(data.incompatibleVehicles[0].make).toEqual('Tesla');
+});
+it('Calls getPowertrainTypeMatch with the correct args', async () => {
+  await fetchVehiclesWithTransformation({
+    cursor: '',
+    direction: '',
+    ownerAddress: '',
+    targetGrantee: '',
+    filters: { powertrainTypes: ['BEV'] },
+  });
+  expect(getPowertrainTypeMatch).toHaveBeenCalledTimes(2);
+  expect((getPowertrainTypeMatch as jest.Mock).mock.calls[0][1]).toEqual(['BEV']);
+  expect((getPowertrainTypeMatch as jest.Mock).mock.calls[1][1]).toEqual(['BEV']);
+});
+it('Returns both vehicles if powertrain types match both', async () => {
+  const data = await fetchVehiclesWithTransformation({
+    cursor: '',
+    direction: '',
+    ownerAddress: '',
+    targetGrantee: '',
+    filters: { powertrainTypes: ['BEV', 'ICE'] },
+  });
+  expect(data.compatibleVehicles.length).toEqual(2);
+});
+it('Only returns vehicles that match BEV powertrain types', async () => {
+  const data = await fetchVehiclesWithTransformation({
+    cursor: '',
+    direction: '',
+    ownerAddress: '',
+    targetGrantee: '',
+    filters: { powertrainTypes: ['BEV'] },
+  });
+  expect(data.compatibleVehicles.length).toEqual(1);
+  expect(data.compatibleVehicles[0].make).toEqual('Tesla');
+  expect(data.incompatibleVehicles.length).toEqual(1);
+  expect(data.incompatibleVehicles[0].make).toEqual('Ford');
+});
+it('Only returns vehicles that match ICE powertrain types', async () => {
+  const data = await fetchVehiclesWithTransformation({
+    cursor: '',
+    direction: '',
+    ownerAddress: '',
+    targetGrantee: '',
+    filters: { powertrainTypes: ['ICE'] },
+  });
+  expect(data.compatibleVehicles.length).toEqual(1);
+  expect(data.compatibleVehicles[0].make).toEqual('Ford');
+  expect(data.incompatibleVehicles.length).toEqual(1);
+  expect(data.incompatibleVehicles[0].make).toEqual('Tesla');
+});
+it('Returns no vehicles if there are no matching powertrain types', async () => {
+  const data = await fetchVehiclesWithTransformation({
+    cursor: '',
+    direction: '',
+    ownerAddress: '',
+    targetGrantee: '',
+    filters: { powertrainTypes: ['HEV'] },
+  });
+  expect(data.compatibleVehicles.length).toEqual(0);
+  expect(data.incompatibleVehicles.length).toEqual(2);
 });
