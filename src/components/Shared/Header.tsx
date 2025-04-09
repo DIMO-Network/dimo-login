f5b13fbaa5b0260a1a017b302a8383df9f865e28
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-04-09 07:25:37 -0500

    fix: update Logo component export to named export for consistency

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,5 +1,5 @@
 import { Back } from "./Back";
-import Logo from "./Logo";
+import { Logo } from "./Logo";
 
 interface HeaderProps {
   title: string;
@@ -16,7 +16,18 @@ export const Header: React.FC<HeaderProps> = ({ title, subtitle, link }) => (
       {(link || subtitle) && (
         <p className="text-sm text-zinc-500 font-medium">
           {link ? (
-            <a href={link} className="underline">
+            <a
+              href={link}
+              className="underline flex flex-row gap-1 justify-center items-center"
+            >
+              <img
+                src={`https://www.google.com/s2/favicons?domain=${
+                  new URL(link).origin
+                }&sz=16`}
+                alt="favicon"
+                className="inline-block ml-1 w-4 h-4"
+                onError={(e) => (e.currentTarget.style.display = "none")}
+              />
               {subtitle}
             </a>
           ) : (
