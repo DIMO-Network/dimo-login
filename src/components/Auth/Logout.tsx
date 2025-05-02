43a09f43f81ea359452c8529395c7357307137af
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-05-01 21:05:43 -0500

    feat: enhance Logout component to manage loading state and utilize dev credentials for logout

--- src/components/Auth/Logout.tsx
+++ src/components/Auth/Logout.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useUIManager } from '../../context/UIManagerContext';
+import { Loader } from '../Shared';
+import { useDevCredentials } from '../../context/DevCredentialsContext';
+import { logout } from '../../utils/authUtils';
 
 export const Logout = () => {
+  const { setLoadingState, setUiState } = useUIManager();
+  const { clientId, redirectUri, utm } = useDevCredentials();
+
+  useEffect(() => {
+    setLoadingState(true, 'Logging out...');
+
+    if (clientId && redirectUri) {
+      logout(clientId, redirectUri, utm, setUiState);
+    }
+  }, [setLoadingState, clientId, redirectUri, utm, setUiState]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">You have been logged out</h1>
-      <p className="text-gray-600">Thank you for using our service.</p>
+      <Loader />
     </div>
   );
 };
