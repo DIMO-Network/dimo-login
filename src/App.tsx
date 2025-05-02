50367752058463560eda3cd5efd7e15a009467c6
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-05-01 20:22:00 -0500

    feat: simplify UI state handling by using a component map and remove unused SELECT_VEHICLES state

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { initializeSession } from './services/sessionService';
 import { useAuthContext } from './context/AuthContext';
-import { useDevCredentials } from './context/DevCredentialsContext'; // Import DevCredentialsContext
+import { useDevCredentials } from './context/DevCredentialsContext';
 import { UiStates, useUIManager } from './context/UIManagerContext';
 
 import {
@@ -30,8 +30,8 @@ import {
 import './App.css';
 
 const App = () => {
-  const { setJwt, setUser, setUserInitialized, userInitialized } = useAuthContext(); // Get loading state from AuthContext
-  const { clientId, apiKey, redirectUri, invalidCredentials } = useDevCredentials(); // Get loading state and credentials from DevCredentialsContext
+  const { setJwt, setUser, setUserInitialized, userInitialized } = useAuthContext();
+  const { clientId, apiKey, redirectUri, invalidCredentials } = useDevCredentials();
   const [email, setEmail] = useState('');
   const { uiState, setUiState, isLoading } = useUIManager();
 
@@ -70,32 +70,31 @@ const App = () => {
     );
   }
 
+  const componentMap: {
+    [key in UiStates]: React.ReactNode;
+  } = {
+    [UiStates.EMAIL_INPUT]: <EmailInput onSubmit={setEmail} />,
+    [UiStates.OTP_INPUT]: <OtpInput email={email} />,
+    [UiStates.PASSKEY_GENERATOR]: <PasskeyGeneration email={email} />,
+    [UiStates.VEHICLE_MANAGER]: <VehicleManager />,
+    [UiStates.MANAGE_VEHICLE]: <ManageVehicle />,
+    [UiStates.ADVANCED_TRANSACTION]: <AdvancedTransaction />,
+    [UiStates.TRANSACTION_SUCCESS]: <SuccessfulTransaction />,
+    [UiStates.VEHICLES_SHARED_SUCCESS]: <SuccessfulPermissions />,
+    [UiStates.ADD_VEHICLE]: <AddVehicle />,
+    [UiStates.COMPATIBILITY_CHECK]: <CompatibilityCheck />,
+    [UiStates.MINT_VEHICLE]: <MintVehicle />,
+    [UiStates.CONNECT_DEVICE]: <ConnectDevice />,
+    [UiStates.CONNECT_TESLA]: <ConnectTesla />,
+    [UiStates.CONNECT_SMARTCAR]: <ConnectSmartCar />,
+    [UiStates.TRANSACTION_CANCELLED]: <CancelledTransaction />,
+    [UiStates.SUCCESS]: <SuccessPage />,
+    [UiStates.LOGOUT]: <Logout />,
+  };
+
   return (
     <div className="flex h-screen pt-2 justify-center bg-white lg-h-screen">
-      {uiState === UiStates.EMAIL_INPUT && <EmailInput onSubmit={setEmail} />}
-      {uiState === UiStates.OTP_INPUT && <OtpInput email={email} />}
-      {uiState === UiStates.PASSKEY_GENERATOR && <PasskeyGeneration email={email} />}
-      {uiState === UiStates.VEHICLE_MANAGER && <VehicleManager />}
-      {uiState === UiStates.MANAGE_VEHICLE && <ManageVehicle />}
-      {uiState === UiStates.ADVANCED_TRANSACTION && <AdvancedTransaction />}
-      {uiState === UiStates.TRANSACTION_SUCCESS && <SuccessfulTransaction />}
-      {uiState === UiStates.VEHICLES_SHARED_SUCCESS && <SuccessfulPermissions />}
-
-      {uiState === UiStates.ADD_VEHICLE && <AddVehicle />}
-
-      {uiState === UiStates.COMPATIBILITY_CHECK && <CompatibilityCheck />}
-
-      {uiState === UiStates.MINT_VEHICLE && <MintVehicle />}
-
-      {uiState === UiStates.CONNECT_DEVICE && <ConnectDevice />}
-
-      {uiState === UiStates.CONNECT_TESLA && <ConnectTesla />}
-
-      {uiState === UiStates.CONNECT_SMARTCAR && <ConnectSmartCar />}
-
-      {uiState === UiStates.TRANSACTION_CANCELLED && <CancelledTransaction />}
-      {uiState === UiStates.SUCCESS && <SuccessPage />}
-      {uiState === UiStates.LOGOUT && <Logout />}
+      {componentMap[uiState] || null}
     </div>
   );
 };
