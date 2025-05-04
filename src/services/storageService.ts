4288efd33414ccf1b9e2671a8bd3cffb06387b05
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-05-04 14:45:50 -0500

    refactor: streamline cookie handling and remove redundant comments in storageService

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -1,22 +1,27 @@
-// Define types for user properties
-
 import { UserObject } from '../models/user';
 
-// Utility function to store JWT in cookies for a given clientId
-export const storeJWTInCookies = (clientId: string, jwt: string): void => {
-  const expirationDate = new Date();
-  expirationDate.setFullYear(expirationDate.getFullYear() + 10); // Set expiration to 10 years in the future
-  let cookieString = `auth_token_${clientId}=${jwt}; expires=${expirationDate.toUTCString()}; path=/`;
+const DEFAULT_COOKIE_EXPIRATION_YEARS = 10; // Default expiration for cookies is 10 years if not specified.
 
-  // Only add Secure and SameSite=None if not on localhost
+export const storeJWTInCookies = (clientId: string, jwt: string): void => {
+  document.cookie = createCookieString(`auth_token_${clientId}`, jwt);
+  document.cookie = createCookieString('dimo_auth_token', jwt);
+};
+
+const createCookieString = (name: string, value: string, expiresAt?: Date): string => {
+  const expirationDate = expiresAt || new Date();
+  if (!expiresAt) {
+    // If no expiration date is provided, cookies are set to expire in 10 years by default.
+    expirationDate.setFullYear(
+      expirationDate.getFullYear() + DEFAULT_COOKIE_EXPIRATION_YEARS,
+    );
+  }
+  let cookieString = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
   if (window.location.hostname !== 'localhost') {
     cookieString += '; SameSite=None; Secure';
   }
-
-  document.cookie = cookieString;
+  return cookieString;
 };
 
-// Utility function to store user properties in localStorage for a given clientId
 export const storeUserInLocalStorage = (
   clientId: string,
   userProperties: UserObject,
@@ -24,7 +29,6 @@ export const storeUserInLocalStorage = (
   localStorage.setItem(`user_data_${clientId}`, JSON.stringify(userProperties));
 };
 
-// Utility function to get JWT from cookies for a given clientId
 export const getJWTFromCookies = (clientId: string): string | null => {
   const cookie = document.cookie
     .split('; ')
@@ -32,13 +36,11 @@ export const getJWTFromCookies = (clientId: string): string | null => {
   return cookie ? cookie.split('=')[1] : null;
 };
 
-// Utility function to get user properties from localStorage for a given clientId
 export const getUserFromLocalStorage = (clientId: string): UserObject | null => {
   const userData = localStorage.getItem(`user_data_${clientId}`);
   return userData ? JSON.parse(userData) : null;
 };
 
-// Utility function to get email granted permission from localStorage for a given clientId
 export const setEmailGranted = (clientId: string, granted: boolean): void => {
   const key = `email_granted_${clientId}`;
   localStorage.setItem(key, JSON.stringify(granted));
@@ -47,13 +49,10 @@ export const setEmailGranted = (clientId: string, granted: boolean): void => {
 export const getEmailGranted = (clientId: string): boolean => {
   const key = `email_granted_${clientId}`;
   const value = localStorage.getItem(key);
-
-  // Parse the stored value or default to `false` if not set
   return value ? JSON.parse(value) : false;
 };
 
-// Utility function to clear the JWT and user data for a given clientId (for logout)
 export const clearSessionData = (clientId: string): void => {
-  document.cookie = `auth_token_${clientId}=; Max-Age=0`; // Expire JWT cookie immediately
-  localStorage.removeItem(`user_data_${clientId}`); // Remove user data from localStorage
+  document.cookie = `auth_token_${clientId}=; Max-Age=0`;
+  localStorage.removeItem(`user_data_${clientId}`);
 };
