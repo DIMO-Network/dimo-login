c2e651581aa955c706aeb7e4407b9074acae770d
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-05-12 21:54:01 -0500

    refactor: simplify SuccessfulPermissions component by removing Card wrapper and consolidating imports

--- src/components/Vehicles/SuccessfulPermissions.tsx
+++ src/components/Vehicles/SuccessfulPermissions.tsx
@@ -1,9 +1,7 @@
 // src/components/SuccessPage.tsx
 import React from 'react';
 
-import Card from '../Shared/Card';
-import Header from '../Shared/Header';
-import PrimaryButton from '../Shared/PrimaryButton';
+import { Header, PrimaryButton } from '../Shared';
 import { useDevCredentials } from '../../context/DevCredentialsContext';
 import { useUIManager } from '../../context/UIManagerContext';
 import { buildAuthPayload } from '../../utils/authUtils';
@@ -39,38 +37,32 @@ export const SuccessfulPermissions: React.FC = () => {
   };
 
   return (
-    <Card
-      width="w-full max-w-[600px]"
-      height="h-fit"
-      className="flex flex-col items-center"
-    >
-      <div className="flex flex-col gap-6 w-[440px]">
-        <Header title={`You have successfully ${action} your vehicles!`} subtitle={''} />
-        <div className="flex flex-col gap-4 max-h-[400px] overflow-auto w-full max-w-[440px]">
-          {vehicles &&
-            vehicles.length > 0 &&
-            vehicles.map((vehicle: Vehicle) => (
-              <VehicleCard
-                key={vehicle.tokenId.toString()}
-                vehicle={vehicle}
-                isSelected={false}
-                onSelect={() => console.log('yee')}
-                disabled={true}
-                incompatible={false}
-              />
-            ))}
-        </div>
-        <div className="flex fex-col">
-          {!isEmbed() && (
-            <div className="flex justify-center">
-              <PrimaryButton onClick={handleBackToThirdParty} width="w-full">
-                Back to {devLicenseAlias}
-              </PrimaryButton>
-            </div>
-          )}
-        </div>
+    <>
+      <Header title={`You have successfully ${action} your vehicles!`} subtitle={''} />
+      <div className="flex flex-col gap-4 max-h-[400px] overflow-auto w-full max-w-[440px]">
+        {vehicles &&
+          vehicles.length > 0 &&
+          vehicles.map((vehicle: Vehicle) => (
+            <VehicleCard
+              key={vehicle.tokenId.toString()}
+              vehicle={vehicle}
+              isSelected={false}
+              onSelect={() => console.log('yee')}
+              disabled={true}
+              incompatible={false}
+            />
+          ))}
       </div>
-    </Card>
+      <div className="flex fex-col">
+        {!isEmbed() && (
+          <div className="flex justify-center w-full">
+            <PrimaryButton onClick={handleBackToThirdParty} width="sm:w-64 w-full">
+              Back to {devLicenseAlias}
+            </PrimaryButton>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
