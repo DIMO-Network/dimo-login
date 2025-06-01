34e37ba5df9355ac7ad14a896142c69c38ff1c85
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-06-01 15:08:24 -0500

    feat: refactor permissions handling with type-safe PERMISSIONS object

--- src/types/permissions.ts
+++ src/types/permissions.ts
@@ -1,6 +1,13 @@
 import { PERMISSIONS as PERMISSIONS_FROM_SDK } from '@dimo-network/transactions';
 
 export type PermissionKey = keyof typeof PERMISSIONS_FROM_SDK;
+export const PERMISSIONS = Object.keys(PERMISSIONS_FROM_SDK).reduce(
+  (acc, key) => {
+    acc[key] = key as PermissionKey;
+    return acc;
+  },
+  {} as Record<string, PermissionKey>,
+) as Record<PermissionKey, PermissionKey>;
 
 export type PermissionsObject = {
   [K in PermissionKey]: boolean;
@@ -8,10 +15,10 @@ export type PermissionsObject = {
 
 // Order is important, as it is used to create the permission string
 export const PERMISSION_KEYS = [
-  PERMISSIONS_FROM_SDK.NONLOCATION_TELEMETRY,
-  PERMISSIONS_FROM_SDK.COMMANDS,
-  PERMISSIONS_FROM_SDK.CURRENT_LOCATION,
-  PERMISSIONS_FROM_SDK.ALLTIME_LOCATION,
-  PERMISSIONS_FROM_SDK.CREDENTIALS,
-  PERMISSIONS_FROM_SDK.STREAMS,
+  PERMISSIONS.NONLOCATION_TELEMETRY,
+  PERMISSIONS.COMMANDS,
+  PERMISSIONS.CURRENT_LOCATION,
+  PERMISSIONS.ALLTIME_LOCATION,
+  PERMISSIONS.CREDENTIALS,
+  PERMISSIONS.STREAMS,
 ] as PermissionKey[];
