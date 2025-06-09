7d1e82b0f087356b488209511443938b7c5565e9
Neil Devas <neil.devas97@gmail.com>
2025-06-09 11:17:58 -0400

    refactor

--- src/components/Auth/PasskeyGeneration.tsx
+++ src/components/Auth/PasskeyGeneration.tsx
@@ -2,34 +2,10 @@ import React, { useEffect, useState, type FC } from 'react';
 
 import { Header } from '../Shared/Header';
 import { PrimaryButton } from '../Shared/PrimaryButton';
-import { DevicesIcon, FingerprintIcon, IconProps, SecurityIcon } from '../Icons';
 import { useAuthContext } from '../../context/AuthContext';
 import { useUIManager } from '../../context/UIManagerContext';
 import ErrorMessage from '../Shared/ErrorMessage';
-
-interface PasskeyBenefitProps {
-  Icon: FC<IconProps>;
-  title: string;
-  description: string;
-}
-
-const PASSKEY_BENEFITS: PasskeyBenefitProps[] = [
-  {
-    Icon: FingerprintIcon,
-    title: 'No need to remember a password',
-    description: 'Passkeys are digital signatures that use Face ID or biometrics.',
-  },
-  {
-    Icon: SecurityIcon,
-    title: 'Advanced protection',
-    description: 'Passkeys offer phishing-resistant technology to keep you safe.',
-  },
-  {
-    Icon: DevicesIcon,
-    title: 'Seamless authentication',
-    description: 'Sign in and approve requests in an instant.',
-  },
-];
+import { Benefits } from '../Passkey/PasskeyBenefits';
 
 interface PasskeyGenerationProps {
   email: string;
@@ -71,32 +47,5 @@ export const PasskeyGeneration: FC<PasskeyGenerationProps> = ({ email }) => {
         </PrimaryButton>
       </div>
     </>
-  );
-};
-
-const Benefits = () => {
-  return (
-    <div className={'flex gap-2 flex-col'}>
-      {PASSKEY_BENEFITS.map((benefitProps) => {
-        return <Benefit {...benefitProps} />;
-      })}
-    </div>
-  );
-};
-
-const Benefit = ({ title, description, Icon }: PasskeyBenefitProps) => {
-  return (
-    <div
-      className="flex flex-col gap-2 w-full p-4 rounded-2xl cursor-pointer transition bg-gray-50 text-gray-500"
-      key={title}
-    >
-      <div className="flex flex-row gap-2 font-medium text-sm text-black">
-        <Icon className="w-5 h-5" />
-        <p>{title}</p>
-      </div>
-      <div className="">
-        <p className="text-sm text-black">{description}</p>
-      </div>
-    </div>
   );
 };
