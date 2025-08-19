90617329a9f274e77f4547aaf94eed994547dfee
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-08-19 08:10:31 -0500

    feat: add permissions check and display note for outdated shared vehicles

--- src/components/Vehicles/index.ts
+++ src/components/Vehicles/index.ts
@@ -4,6 +4,7 @@ export * from './EmptyState';
 export * from './ManageVehicle';
 export * from './MintVehicle';
 export * from './SelectVehicles';
+export * from './SharedPermissionsNote';
 export * from './SuccessfulPermissions';
 export * from './VehicleCard';
 export * from './VehicleManager';
