0cddd1a1dba2a14c7a6bf57609ee86d5b44605ff
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-10-20 19:00:43 +0200

    [DVR-745] read configuration from dev console using configuration id

---
+++ src/services/configurationService.ts
@@ -0,0 +1,22 @@
+export interface Configuration {
+  id: string;
+  client_id: string;
+  configuration: Record<string, string | number | boolean>;
+}
+
+export async function getConfigurationById(configurationId: string): Promise<Configuration>{
+  try {
+    const response = await fetch(`${process.env.REACT_APP_DEVELOPER_CONSOLE_URL}/api/configurations/${configurationId}`);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const remoteConfiguration: { configuration: Configuration } = await response.json();
+
+    return remoteConfiguration.configuration;
+  } catch (e: unknown){
+    console.error(e);
+    throw e;
+  }
+}
